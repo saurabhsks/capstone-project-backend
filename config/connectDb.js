47c0eb3acc5e6c8eb1e8772e0164e9d23620bbd7
Saurabh <saurabhsingh982001@gmail.com>
2023-01-30 13:15:13 +0530

    commit changes

--- config/connectDb.js
+++ config/connectDb.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect(`mongodb://127.0.0.1:27017/products`)
+         await mongoose.connect(`mongodb://127.0.0.1:27017/products`)
+       // await mongoose.connect(process.env.DBurl)
         console.log('Database connected sucessfully')
     }
     catch (error) {
